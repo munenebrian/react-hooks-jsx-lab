fda863682adc5d92716a0133714000b999ef9563
munenebrian <njerubrianmunene@gmail.com>
2022-07-13 10:01:22 +0300

    assignment done, some chekers unable

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,17 @@ import Home from "./Home";
 import About from "./About";
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-    </div>
-  );
+    return ( <
+        div >
+        <
+        NavBar / >
+        <
+        Home / >
+        <
+        About / >
+        <
+        /div>
+    );
 }
 
 export default App;
